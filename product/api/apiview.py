1dbc9dcf93a26188108d929d2d630e5cec3df04b
surajnai567 <53654029+surajnai567@users.noreply.github.com>
2020-09-15 16:30:19 +0530

    Update apiview.py
    url

--- product/api/apiview.py
+++ product/api/apiview.py
@@ -9,7 +9,7 @@ class ProductApiView(APIView):
 		products = Product.objects.all()
 		data = ProductSerializer(products, many=True).data
 		for da in data:
-			da['imageUrl'] = "http://192.168.43.222:8080" + da['imageUrl']
+			da['imageUrl'] = "https://jshop-api-backend.herokuapp.com/" + da['imageUrl']
 			sub = SubCategory.objects.filter(id=da['subCategory'])
 			if(len(sub)):
 				da['subCatName'] = sub[0].name
