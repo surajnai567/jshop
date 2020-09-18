4bc28dbb3522d90ceaea1d5ceec615c6559f26c0
surajnai567 <surajnai567@gmail.com>
2020-09-18 22:48:16 +0530

    apiview

--- product/api/apiview.py
+++ product/api/apiview.py
@@ -2,6 +2,8 @@ from .serializer import ProductSerializer
 from rest_framework.views import APIView, Response
 from product.models import Product
 from subcategory.models import SubCategory
+from pricelist.models import PriceList
+import math
 
 
 class ProductApiView(APIView):
@@ -11,8 +13,15 @@ class ProductApiView(APIView):
 		for da in data:
 			da['imageUrl'] = "https://jshop-api-backend.herokuapp.com" + da['imageUrl']
 			sub = SubCategory.objects.filter(id=da['subCategory'])
+			weight = da['weight']
+			priceobj = PriceList.objects.filter(subcategory=da['subCategory'])[0]
+			discount = priceobj.discount
 			if(len(sub)):
 				da['subCatName'] = sub[0].name
+				da['MRP'] = math.ceil(float(weight) / 10 * int(priceobj.amount))
+				da['discount'] = discount
+				da['sellMRP'] = math.ceil(float(weight) / 10 * int(priceobj.amount) - (
+						float(weight) / 10 * int(priceobj.amount)) * int(discount) / 100)
 		return Response(data)
 
 
@@ -23,6 +32,12 @@ class ProductApiViewCat(APIView):
 		for da in data:
 			da['imageUrl'] = "https://jshop-api-backend.herokuapp.com" + da['imageUrl']
 			sub = SubCategory.objects.filter(id=da['subCategory'])
+			weight = da['weight']
+			priceobj = PriceList.objects.filter(subcategory=da['subCategory'])[0]
+			discount = priceobj.discount
 			if (len(sub)):
 				da['subCatName'] = sub[0].name
+				da['MRP'] = math.ceil(float(weight)/10*int(priceobj.amount))
+				da['discount'] = discount
+				da['sellMRP'] = math.ceil(float(weight)/10*int(priceobj.amount) - (float(weight)/10*int(priceobj.amount))*int(discount)/100)
 		return Response(data)
